e3129e53067e69973710ab89b2d48242380192e9
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-22 17:32:05 -0300

    Asigno las rutas a los ítems del menú.

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -18,15 +18,18 @@ export class MenuComponent implements OnInit {
                 items: [
                     {
                       label: 'Textos y Fechas',
-                      icon: 'pi pi-align-left'
+                      icon: 'pi pi-align-left',
+                      routerLink: 'basics'
                     },
                     {
                       label: 'Números',
-                      icon: 'pi pi-dollar'
+                      icon: 'pi pi-dollar',
+                      routerLink: 'numbers'
                     },
                     {
                       label: 'No comúnes',
-                      icon: 'pi pi-globe'
+                      icon: 'pi pi-globe',
+                      routerLink: 'uncommon'
                     }
                 ]
             },
