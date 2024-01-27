0def7ff9eeeabf07bfc8d0f493cb3efeb7d04e24
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-27 20:12:02 -0300

    Pipe i18n select

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18n Select
+  public name: string = 'Sebastián';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Camila';
+    this.gender = 'female';
+  }
 }
