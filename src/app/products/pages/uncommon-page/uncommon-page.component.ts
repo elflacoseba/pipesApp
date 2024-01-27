b6050260f6d5512048b95a383fe2f38e00730928
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-27 20:25:26 -0300

    Pipe i18n Plural

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -19,4 +19,17 @@ export class UncommonPageComponent {
     this.name = 'Camila';
     this.gender = 'female';
   }
+
+  //i18n Plural
+  public clients: string[] = ['María', 'Pedro', 'Fernando'];
+  public clientsMap = {
+    '=0': 'no tenemos clientes esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
 }
