b6c8e6b8c20f10ced72b6f09696036548458fae8
Sebastián Serrisuela <sebaserri@gmail.com>
2024-01-31 20:39:39 -0300

    Agrego botones y paso el argumento al pipe al hacer click en un botón.

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,7 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class ToggleCasePipe implements PipeTransform {
-  transform(value: string): string {
-    return value.toUpperCase();
+  transform (value: string, toUpper: boolean = false ): string {
+
+    if (toUpper)
+      return value.toUpperCase();
+    else
+      return value.toLowerCase();
   }
 }
